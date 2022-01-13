9684075f45a0c623f1d1c533ec4a2e2d2b546969
hoaklee <hoaklee@outlook.com>
2022-01-13 09:54:05 +0100

    fix setup bug

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-import gym
-import highway_env
+# Following PEP 517/518, this file should not not needed and replaced instead by the setup.cfg file and pyproject.toml.
+# Unfortunately it is still required py the pip editable mode `pip install -e`
+# See https://stackoverflow.com/a/60885212
 
-env = gym.make("highway-v0")
+from setuptools import setup
 
-done = False
-obs_list = []
-while not done:
-    action = [-0.2, 0]
-    obs, reward, done, info = env.step(action)
-    obs_list.append(reward)
-    env.render()
-print(obs_list)
+if __name__ == "__main__":
+    setup()
